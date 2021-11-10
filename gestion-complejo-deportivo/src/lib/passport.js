3f30b45a8324b2eef104cff0742818ce57ee267f
elFonTii <feluu.contacto@gmail.com>
2021-11-10 20:51:30 -0300

    Validate characters in username
    El sistema verifica si el usaername contiene caracteres especiales, como un espacio o cualquier otro.

--- gestion-complejo-deportivo/src/lib/passport.js
+++ gestion-complejo-deportivo/src/lib/passport.js
@@ -15,7 +15,7 @@ passport.use('local.signin', new LocalStrategy({
     const user = rows[0];
     const profile_img = pool.query('SELECT src FROM profile_img INNER JOIN users ON profile_img.id_img = users.profile WHERE username = ?', [user.username]);
     const validPassword = await helpers.matchPassword(password, user.password);
-    
+
     if (validPassword) {
       done(null, user, req.flash('success', 'Bienvenido a la plataforma ' + user.name));
     } else {
@@ -33,44 +33,51 @@ passport.use('local.signup', new LocalStrategy({
   passReqToCallback: true
 }, async (req, username, password, done) => {
   const { email, name, surname, localidad, direccion, nacimiento, gen } = req.body;
-  
+
   //Verify if the user already exists
   const users = await pool.query('SELECT * FROM users WHERE username = ? OR email = ?', [username, email]);
   if (users.length > 0) {
     return done(null, false, req.flash('message', 'Ya existe un usuario con ese usuario o correo.'));
   } else {
-    if(gen == 1)
-      profile = 'profile_picture_2.svg';
-    else
-      profile = 'profile_picture_girl';
-      
-  let newUser = {
-    email,
-    name,
-    surname,
-    username,
-    nacimiento,
-    direccion,
-    localidad,
-    profile,
-    password,
-    gen,
-    rol: 2
-  };
-  newUser.password = await helpers.encryptPassword(password);
-  // Saving in the Database
-  const result = await pool.query('INSERT INTO users SET ? ', newUser);
-  newUser.id = result.insertId;
-  return done(null, newUser);
+    //Verify if the username have invalid characters.
+    const acceptedCharacter = /^[a-zA-Z0-9]+$/;
+    if (!acceptedCharacter.test(username)) {
+      return done(null, false, req.flash('message', 'El nombre de usuario no puede contener caracteres especiales.'));
+    } else {
+
+      if (gen == 1)
+        profile = 'profile_picture_2.svg';
+      else
+        profile = 'profile_picture_girl';
+
+      let newUser = {
+        email,
+        name,
+        surname,
+        username,
+        nacimiento,
+        direccion,
+        localidad,
+        profile,
+        password,
+        gen,
+        rol: 2
+      };
+      newUser.password = await helpers.encryptPassword(password);
+      // Saving in the Database
+      const result = await pool.query('INSERT INTO users SET ? ', newUser);
+      newUser.id = result.insertId;
+      return done(null, newUser);
+    }
   }
 }));
 
 // Serialize the user to the session
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
 // Deserialize the user from the session
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   done(null, user);
 });
